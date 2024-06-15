3011c1ad93e323c935bcbd24be545066576f88e2
11809FahrezaAP <85097059+11809FahrezaAP@users.noreply.github.com>
2024-06-15 09:11:55 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-Route::get('/bukutampil', 'BukuController@bukutampil');
+Route::get('/buku', 'BukuController@bukutampil');
 Route::get('/home',function(){return view('view_home');});
 Route::get('/anggota', 'AnggotaController@anggotatampil');
 Route::get('/petugas', 'PetugasController@petugastampil');
