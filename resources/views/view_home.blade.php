3ea3173755155fab773938857f294ebd1fe42a47
11809FahrezaAP <85097059+11809FahrezaAP@users.noreply.github.com>
2024-06-15 08:40:04 +0700

    setengah jalan

---
+++ resources/views/view_home.blade.php
@@ -0,0 +1,28 @@
+@extends('index')
+@section('title', 'Home')
+
+@section('isihalaman')
+    <h3>Pengertian perpustakaan menurut para ahli</h3>
+    <p>
+    <h4>Perpustakaan</h4>
+    Menurut UU Perpustakaan pada Bab I pasal 1 menyatakan Perpustakaan adalah institusi yang mengumpulkan pengetahuan tercetak dan terekam, 
+    mengelolanya dengan cara khusus guna memenuhi kebutuhan intelektualitas para penggunanya melalui beragam cara interaksi pengetahuan.
+    <p>
+    <h4>Peran Perpustakaan</h4>
+    Perpustakaan merupakan upaya untuk memelihara dan meningkattkan efisiensi dan efektifitas proses belajar-mengajar. 
+    Perpustakaan yang terorganisir secara baik dan sisitematis, secara langsung atau pun tidak langsung dapat memberikan kemudahan bagi proses belajar mengajar di sekolah tempat perpustakaan tersebut berada. Hal ini, 
+    trekait dengan kemajuan bidang pendidikan dan dengan adanya perbaikan metode belajar-mengajar yang dirasakan tidak bisa dipisahkan dari masalah penyediaan fasilitas dan sarana pendidikan. 
+
+    <p>
+    Tujuan perpustakaan adalah untuk membantu masyarakat dalam segala umur dengan memberikan kesempatan dengan dorongan melelui jasa pelayanan perpustakaan agar mereka: 
+    a. Dapat mendidik dirinya sendiri secara berkesimbungan; 
+    b. Dapat tanggap dalam kemajuan pada berbagai lapangan ilmu pengetahuan, kehidupan sosial dan politik; 
+    c. Dapat memelihara kemerdekaan berfikir yang konstruktif untuk menjadi anggota keluarga dan masyarakat yang lebih baik; 
+    d. Dapat mengembangkan kemampuan berfikir kreatif, membina rohani dan dapat menggunakan kemempuannya untuk dapat menghargai hasil seni dan budaya manusia; 
+    e. Dapat meningkatkan tarap kehidupan seharihari dan lapangan pekerjaannya; 
+    f. Dapat menjadi warga negara yang baik dan dapat berpartisipasi secara aktif dalam pembangunan nasional dan dalam membina saling pengertian antar bangsa; 
+    g. Dapat menggunakan waktu senggang dengan baik yang bermanfaat bagi kehidupan pribadi dan sosial
+    <p>
+    <h4>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/buku', 'BukuController@bukutampil');
+Route::get('/home',function(){return view('view_home');});
+Route::get('/anggota', 'AnggotaController@anggotatampil');
+Route::get('/petugas', 'PetugasController@bukutampil');
+Route::get('/pinjam', 'PinjamController@bukutampil');
